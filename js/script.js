445295d643f611e068456906e84f46d0b9914a88
shubham Sarode <shubhamsarode435@gmail.com>
2022-02-28 09:04:25 +0530

    commit
    change the commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+// $(window).scroll(function(){
+//     if ($(window).scrollTop() >= 100) {
+//         $('#header').addClass('fixed');
+       
+//     }
+//     else {
+//         $('#header').removeClass('fixed');
+        
+//     }
+// });
+
+
