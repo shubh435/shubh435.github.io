cebf479150cb9418357d9e3b6f6c8cc6f031e1e6
shubham <intern.shubham@sangnak.in>
2022-04-21 20:37:19 +0530

    ..

--- js/script.js
+++ js/script.js
@@ -1,14 +1,2 @@
 
 
-// $(window).scroll(function(){
-//     if ($(window).scrollTop() >= 100) {
-//         $('#header').addClass('fixed');
-       
-//     }
-//     else {
-//         $('#header').removeClass('fixed');
-        
-//     }
-// });
-
-
