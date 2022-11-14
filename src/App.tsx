807deefb8bae0581003974ef7e9ac7fa89cf7515
shubham sarode <shubham.sarode@extwebtech.in>
2022-11-14 22:37:13 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import DrawerAppBar from "./components/Appbar";
+
+function App() {
+  return (
+    <>
+      <DrawerAppBar />
+    </>
+  );
+}
+
+export default App;
