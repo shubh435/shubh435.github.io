d24a45ca1974c399581580d4bbde64e27de25b1e
shubham sarode <shubham.sarode@extwebtech.in>
2023-01-17 09:03:46 +0530

    created and implemented custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { ThemeProvider } from "@mui/system";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import RouteToNavigate from "./routes";
+import theme from "./Theme/Theme";
 
 function App() {
   return (
     <BrowserRouter>
-      <RouteToNavigate />
+      <ThemeProvider theme={theme}>
+        <RouteToNavigate />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
